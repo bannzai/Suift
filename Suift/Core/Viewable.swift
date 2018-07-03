e7b9ca97934cee6a0cc07882543a2eec622fb595
bannzai <kingkong999yhirose@gmail.com>
2018-07-03 13:32:04 +0900

    :recycle: refactor core structure

---
+++ Suift/Core/Viewable.swift
@@ -0,0 +1,13 @@
+//
+//  Viewable.swift
+//  Suift
+//
+//  Created by Yudai.Hirose on 2018/07/03.
+//  Copyright © 2018年 廣瀬雄大. All rights reserved.
+//
+
+import Foundation
+
+public protocol Viewable {
+    func view() -> UIView
+}
