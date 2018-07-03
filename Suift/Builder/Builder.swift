c40e6f497e01038f7c3013deec05202a65f02cdd
bannzai <kingkong999yhirose@gmail.com>
2018-07-03 12:02:45 +0900

    :metal: create buildable

---
+++ Suift/Builder/Builder.swift
@@ -0,0 +1,16 @@
+//
+//  Builder.swift
+//  Suift
+//
+//  Created by Yudai.Hirose on 2018/07/03.
+//  Copyright © 2018年 廣瀬雄大. All rights reserved.
+//
+
+import Foundation
+
+public protocol Buildable {
+    associatedtype View: UIView
+    func build() -> View
+}
+
+
