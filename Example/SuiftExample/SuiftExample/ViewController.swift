aed3323142b359727b98e7dbe1bb0bb8b07cff2c
bannzai <kingkong999yhirose@gmail.com>
2018-07-04 01:00:59 +0900

    :metal: fix example

--- Example/SuiftExample/SuiftExample/ViewController.swift
+++ Example/SuiftExample/SuiftExample/ViewController.swift
@@ -20,19 +20,17 @@ class ViewController: UIViewController {
 extension ViewController: Buildable {
     func build() -> Viewable {
         return View(
-            structure: ViewStructure(
-                style: ViewStyle {
-                    $0.backgroundColor = Color.RGB(red: 255 / 255, green: 100 / 255, blue: 80 / 255)
-                },
-                constraint: LayoutMaker {
-                    return [
-                        Layout { $0.centerXAnchor.constraint(equalTo: self.view.centerXAnchor) },
-                        Layout { $0.centerYAnchor.constraint(equalTo: self.view.centerYAnchor) },
-                        Layout { $0.widthAnchor.constraint(equalToConstant: 100) },
-                        Layout { $0.heightAnchor.constraint(equalToConstant: 100) },
-                        ]
-                }
-            )
+            style: ViewStyle {
+                $0.backgroundColor = Color.RGB(red: 255 / 255, green: 100 / 255, blue: 80 / 255)
+            },
+            constraint: LayoutMaker {
+                return [
+                    Layout { $0.centerXAnchor.constraint(equalTo: self.view.centerXAnchor) },
+                    Layout { $0.centerYAnchor.constraint(equalTo: self.view.centerYAnchor) },
+                    Layout { $0.widthAnchor.constraint(equalToConstant: 100) },
+                    Layout { $0.heightAnchor.constraint(equalToConstant: 100) },
+                    ]
+            }
         )
     }
 }
