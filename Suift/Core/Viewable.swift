c45fbb7fb7525578412a34c685a2961590759f81
bannzai <kingkong999yhirose@gmail.com>
2018-07-04 23:54:01 +0900

    :sparkles: add default functions and necessary property

--- Suift/Core/Viewable.swift
+++ Suift/Core/Viewable.swift
@@ -9,6 +9,10 @@
 import Foundation
 
 public protocol Viewable {
+    var style: ViewStyle { get }
+    var constraint: LayoutMaker { get }
+    var children: [Viewable] { get }
+    
     func stylize()
     func layout()
     func activateChildren()
@@ -18,6 +22,29 @@ public protocol Viewable {
 }
 
 extension Viewable {
+    public func stylize() {
+        let view = self.view()
+        style.apply(with: view)
+    }
+    
+    public func layout() {
+        let view = self.view()
+        let set: ViewSetForLayout = (view, view.superview!, view.superview!.subviews)
+        let layouts = constraint.layout(set: set)
+        view.translatesAutoresizingMaskIntoConstraints = layouts.isEmpty
+        NSLayoutConstraint.activate( layouts )
+    }
+    
+    public func activateChildren() {
+        let view = self.view()
+        children
+            .forEach {
+                let child = $0.view()
+                view.addSubview(child)
+                $0.activate()
+        }
+    }
+    
     public func activate() {
         stylize()
         layout()
