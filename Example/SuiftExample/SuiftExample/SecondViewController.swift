4d2ec488aaefdb4edc73b994038b4567591d749a
bannzai <kingkong999yhirose@gmail.com>
2018-07-05 13:26:18 +0900

    :metal: Successfully button event

--- Example/SuiftExample/SuiftExample/SecondViewController.swift
+++ Example/SuiftExample/SuiftExample/SecondViewController.swift
@@ -7,29 +7,57 @@
 //
 
 import UIKit
+import Suift
 
 class SecondViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        render()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    deinit {
+        print(#function)
     }
-    
 
-    /*
-    // MARK: - Navigation
+}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+extension SecondViewController: Buildable {
+    func build() -> Viewable {
+        return View(
+            style: ViewStyle { $0.backgroundColor = UIColor.white },
+            constraint: LayoutMaker { view, superview, views in
+                return [
+                    { view.topAnchor.constraint(equalTo: superview.topAnchor) },
+                    { view.bottomAnchor.constraint(equalTo: superview.bottomAnchor) },
+                    { view.leftAnchor.constraint(equalTo: superview.leftAnchor) },
+                    { view.rightAnchor.constraint(equalTo: superview.rightAnchor) },
+                ]
+            },
+            children: [
+                Button(
+                    style: ButtonStyle {
+                        $0.viewStyle = ViewStyle {
+                            $0.backgroundColor = UIColor.red
+                        }
+                    },
+                    constraint: LayoutMaker { view, superview, views in
+                        return [
+                            view.centerXAnchor.constraint(equalTo: superview.centerXAnchor) ,
+                            view.centerYAnchor.constraint(equalTo: superview.centerYAnchor) ,
+                            view.widthAnchor.constraint(equalToConstant: 200),
+                            view.heightAnchor.constraint(equalToConstant: 50),
+                            ]
+                    },
+                    event: ButtonEvent(
+                        events: .touchUpInside,
+                        closure: { [weak self] (button) in
+                            self?.navigationController?.pushViewController(SecondViewController(), animated: true)
+                        }
+                    )
+                )
+            ]
+        )
     }
-    */
-
 }
