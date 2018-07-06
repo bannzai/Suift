2ea6f959efbebac4855c3beecbb2e912c801e0b9
bannzai <kingkong999yhirose@gmail.com>
2018-07-06 22:26:41 +0900

    :metal: add isEqual for optional

--- Suift/StdLib/Equatable.swift
+++ Suift/StdLib/Equatable.swift
@@ -11,15 +11,66 @@ import Foundation
 public typealias SuiftEquatable = WrapperForEquatable & Equatable
 
 public protocol WrapperForEquatable {
-    func isEqual<T>(compare: T) -> Bool
+    func _isEqual(compare: Any) -> Bool
 }
 
 extension WrapperForEquatable where Self: Equatable {
-    public func isEqual<T>(compare: T) -> Bool {
-        return _isEqual(compare: compare as! Self)
+    public func _isEqual(compare: Any) -> Bool {
+        return isEqual(compare: compare as! Self)
     }
     
-    public func _isEqual(compare: Self) -> Bool {
+    public func isEqual(compare: Self) -> Bool {
         return self == compare
+    }
+}
+
+// FIXME: 1. Cannot used this function
+//    func isEqualForOptional<T>(lhs: T?, rhs: T?) -> Bool where T: WrapperForEquatable {
+//        return lhs.isEqualForOptional(compare: rhs)
+//    }
+
+// FIXME: 2. Cannot declear with generics
+//struct OptoinalEqual<T: WrapperForEquatable> {
+
+// FIXME: 3. Cannot use this extension... fmmm
+//extension Optional: WrapperForEquatable where Wrapped: WrapperForEquatable {
+//    public func _isEqual(compare: Any) -> Bool {
+//        return isEqualForOptional(compare: compare as? Wrapped)
+//    }
+//
+//    internal func isEqualForNotOptional(compare: Wrapped) -> Bool {
+//        return isEqualForOptional(compare: Optional(compare))
+//    }
+//
+//    internal func isEqualForOptional(compare: Optional<Wrapped>) -> Bool {
+//        switch (self, compare) {
+//        case (let lhs?, let rhs?):
+//            return lhs._isEqual(compare: rhs)
+//        case (nil, nil):
+//            return true
+//        case (_, nil):
+//            return false
+//        case (nil, _):
+//            return false
+//        }
+//    }
+//}
+
+// For compare to optional
+struct OptoinalEqual<T> {
+    static func isEqual(_ lhs: T, _ rhs: T) -> Bool {
+        let optional1 = lhs as? WrapperForEquatable
+        let optional2 = rhs as? WrapperForEquatable
+        
+        switch (optional1, optional2) {
+        case (let lhs?, let rhs?):
+            return lhs._isEqual(compare: rhs)
+        case (nil, nil):
+            return true
+        case (_, nil):
+            return false
+        case (nil, _):
+            return false
+        }
     }
 }
