59ad38120226d4b50244ad06cde32597f83b933f
bannzai <kingkong999yhirose@gmail.com>
2018-07-07 10:56:35 +0900

    :metal: add CollectionViewItem

--- Suift/View/CollectionView/DataSource/CollectionViewItem.swift
+++ Suift/View/CollectionView/DataSource/CollectionViewItem.swift
@@ -1,5 +1,5 @@
 //
-//  CollectionViewItem.swift
+//  CollectionViewCollectionViewItem.swift
 //  Suift
 //
 //  Created by Yudai.Hirose on 2018/07/07.
@@ -7,3 +7,34 @@
 //
 
 import Foundation
+
+public protocol CollectionViewItemType {
+    var reusableIdentifier: String { get set }
+}
+
+public protocol CollectionViewItemDelegatable {
+    func configureCell(collectionView: UICollectionView, cell: UICollectionViewCell, indexPath: IndexPath)
+    func sizeFor(collectionView: UICollectionView, indexPath: IndexPath) -> CGSize
+}
+
+public struct CollectionViewItem<T: UICollectionViewCell>: CollectionViewItemType {
+    public typealias CollectionViewItemInfomation = (CollectionViewItem: CollectionViewItem<T>, collectionView: UICollectionView, indexPath: IndexPath)
+    public var reusableIdentifier: String
+    
+    public var configureCell: ((T, CollectionViewItemInfomation) -> Void)?
+    public var sizeFor: ((CollectionViewItemInfomation) -> CGSize)?
+}
+
+extension CollectionViewItem: CollectionViewItemDelegatable {
+    public func configureCell(collectionView: UICollectionView, cell: UICollectionViewCell, indexPath: IndexPath) {
+        configureCell?(cell as! T, (self, collectionView, indexPath))
+    }
+    
+    public func sizeFor(collectionView: UICollectionView, indexPath: IndexPath) -> CGSize {
+        guard let size = sizeFor?((self, collectionView, indexPath)) else {
+            return .zero
+        }
+        
+        return size
+    }
+}
