4198ea2bb93a1a469a96ccf956bde0a49ea998a3
bannzai <kingkong999yhirose@gmail.com>
2018-07-09 18:48:50 +0900

    :sparkles: add section inializer for items closure with elements

--- Suift/View/CollectionView/CollectionViewSection.swift
+++ Suift/View/CollectionView/CollectionViewSection.swift
@@ -55,6 +55,28 @@ public struct CollectionViewSectionImpl: CollectionViewSection {
         self.minimumInteritemSpacing = minimumInteritemSpacing
     }
     
+    public init<E>(
+        elements: [E],
+        itemsClosure: ((E) -> [CollectionViewItem]),
+        
+        header: CollectionViewSectionHeaderFooterViewable? = nil,
+        footer: CollectionViewSectionHeaderFooterViewable? = nil,
+        
+        inset: ((SectionArgument) -> UIEdgeInsets)? = nil,
+        minimumLineSpacing: ((SectionArgument) -> CGFloat)? = nil,
+        minimumInteritemSpacing: ((SectionArgument) -> CGFloat)? = nil
+        ) {
+        self.items = elements.flatMap { itemsClosure($0) }
+        
+        self.header = header
+        self.footer = footer
+        
+        self.inset = inset
+        self.minimumLineSpacing = minimumLineSpacing
+        self.minimumInteritemSpacing = minimumInteritemSpacing
+    }
+
+    
     public mutating func remove(for item: Int) -> CollectionViewItem {
         return items.remove(at: item)
     }
