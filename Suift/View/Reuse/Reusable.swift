93410a9d0162917ae5a28a9ad302db9df206ec35
bannzai <kingkong999yhirose@gmail.com>
2018-07-09 16:43:17 +0900

    :metal: add Reusable

---
+++ Suift/View/Reuse/Reusable.swift
@@ -0,0 +1,13 @@
+//
+//  Reusable.swift
+//  Suift
+//
+//  Created by Yudai.Hirose on 2018/07/09.
+//  Copyright © 2018年 廣瀬雄大. All rights reserved.
+//
+
+import Foundation
+
+public protocol Reusable {
+    var identifier: String { get }
+}
