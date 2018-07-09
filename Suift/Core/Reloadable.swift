0f0c0266e657a043ec0aea96450cf97f94494fc1
bannzai <kingkong999yhirose@gmail.com>
2018-07-09 19:39:38 +0900

    :metal: add Relodable

---
+++ Suift/Core/Reloadable.swift
@@ -0,0 +1,17 @@
+//
+//  Reloadable.swift
+//  Suift
+//
+//  Created by Yudai.Hirose on 2018/07/09.
+//  Copyright © 2018年 廣瀬雄大. All rights reserved.
+//
+
+import Foundation
+
+public protocol Reloadable: class {
+    func reload()
+}
+
+public protocol CollectionViewReloadable: Reloadable, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+}
