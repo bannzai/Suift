d1933a5f4bbbc0ec3fe475184c6a62bc8872ca74
bannzai <kingkong999yhirose@gmail.com>
2018-07-18 21:38:10 +0900

    :sparkles: add ViewActivatable

---
+++ Suift/Core/ViewActivatable.swift
@@ -0,0 +1,13 @@
+//
+//  ViewActivatable.swift
+//  Suift
+//
+//  Created by Yudai.Hirose on 2018/07/18.
+//  Copyright © 2018年 廣瀬雄大. All rights reserved.
+//
+
+import Foundation
+
+public protocol ViewActivatable {
+    func activate(for view: UIView, viewUpdateSet set: ViewUpdateSet)
+}
