956f2d0300527e6b876d260fca939d92b20b52cc
bannzai <kingkong999yhirose@gmail.com>
2018-07-19 23:42:59 +0900

    :metal: Add function for view layoutable

--- Suift/Core/ViewLayoutable.swift
+++ Suift/Core/ViewLayoutable.swift
@@ -9,5 +9,15 @@
 import Foundation
 
 public protocol ViewLayoutable {
+    var constraint: LayoutMaker { get }
     func layout(for view: UIView)
+}
+
+extension ViewLayoutable {
+    public func layout(for view: UIView) {
+        let set: ViewSetForLayout = (view, view.superview!, view.superview!.subviews)
+        let layouts = constraint.layout(set: set)
+        view.translatesAutoresizingMaskIntoConstraints = layouts.isEmpty
+        NSLayoutConstraint.activate( layouts )
+    }
 }
