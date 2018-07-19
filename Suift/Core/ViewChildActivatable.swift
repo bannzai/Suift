965abc39b2cbe403ae3c8c1407dbae611a8c325b
bannzai <kingkong999yhirose@gmail.com>
2018-07-19 23:42:45 +0900

    :metal: Add function for child activatabl3

--- Suift/Core/ViewChildActivatable.swift
+++ Suift/Core/ViewChildActivatable.swift
@@ -9,5 +9,19 @@
 import Foundation
 
 public protocol ViewChildActivatable {
+    var children: [ViewChildable] { get }
     func activateChildren(for view: UIView, parentViewUpdateSet set: ViewUpdateSet)
+}
+
+extension ViewChildActivatable where Self: ViewUpdateDecidable {
+    public func activateChildren(for view: UIView, parentViewUpdateSet set: ViewUpdateSet) {
+        children
+            .forEach {
+                let child = $0.view()
+                if child.superview == nil {
+                    view.addSubview(child)
+                }
+                $0.activate(for: child, viewUpdateSet: mixViewUpdateStatus(parent: set))
+        }
+    }
 }
