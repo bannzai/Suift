d7dfa1a3965511c35551ec8bbc8f3e62969252ed
bannzai <kingkong999yhirose@gmail.com>
2018-07-19 21:36:28 +0900

    :bug: Fix to use footer

--- Suift/View/CollectionView/CollectionViewSource+UICollectionViewDataSource.swift
+++ Suift/View/CollectionView/CollectionViewSource+UICollectionViewDataSource.swift
@@ -252,7 +252,7 @@ extension CollectionViewSource: UICollectionViewDelegateFlowLayout {
     }
     
     public func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForFooterInSection section: Int) -> CGSize {
-        if let footer = sections[section].header,
+        if let footer = sections[section].footer,
             let size = sectionHeaderFooterSizeFor(headerFooter: footer, collectionView: collectionView, section: section) {
             return size
         }
